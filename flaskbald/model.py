94473d5a57f230007e0dc4c7d874138186cec8d6
Jim Myers <jfmyers01@gmail.com>
2018-02-01 09:21:12 -0500

    Remove id from model.py

--- flaskbald/model.py
+++ flaskbald/model.py
@@ -62,7 +62,6 @@ def create_model(db):
 
         __abstract__  = True
 
-        id            = db.Column(db.Integer, primary_key=True)
         date_created  = db.Column(db.DateTime,  default=db.func.current_timestamp())
         date_modified = db.Column(db.DateTime,  default=db.func.current_timestamp(),
                                                   onupdate=db.func.current_timestamp())
